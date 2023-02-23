f694726f048dca1f6b5e816bd84f23a3bfb97bf8
Pedro Furlan <pedrofurlan6@gmail.com>
2023-02-23 12:00:59 -0300

    Create Application structure

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export function Groups() {
+  return (
+    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1}}>
+      <Text>Groups</Text>
+    </View>
+  )
+} 
