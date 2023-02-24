e116e1bef715546d78c7b6a52d2c33390db42134
Pedro Furlan <pedrofurlan6@gmail.com>
2023-02-24 11:46:05 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { Groups } from '@screens/Groups';
-import { ThemeProvider } from 'styled-components/native';
-import defaultTheme from './src/theme/defaultTheme';
-
+import { Groups } from "@screens/Groups";
+import { ThemeProvider } from "styled-components/native";
+import defaultTheme from "./src/theme/defaultTheme";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={defaultTheme}>
-      <StatusBar />
-
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
-  )
-};
+  );
+}
