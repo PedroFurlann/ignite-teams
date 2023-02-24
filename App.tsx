0988cec2ebbe032d0b316e8b1942da303628301d
Pedro Furlan <pedrofurlan6@gmail.com>
2023-02-24 10:14:44 -0300

    Create defaultTheme

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { Groups } from '@screens/Groups';
+import { ThemeProvider } from 'styled-components/native';
+import defaultTheme from './src/theme/defaultTheme';
+
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <StatusBar />
 
       <Groups />
-    </>
+    </ThemeProvider>
   )
 };
