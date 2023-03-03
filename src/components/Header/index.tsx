72b2c50d44e54b1944140b71238f6ea758770a3f
Pedro Furlan <pedrofurlan6@gmail.com>
2023-03-03 10:45:53 -0300

    Implementing BackButton route

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,23 @@
 import { Container, IconBtn, LeftArrowIcon, Logo } from "./styles";
 
 import logo from '@assets/logo.png'
+import { useNavigation } from "@react-navigation/native";
 
 interface HeaderProps {
   showIconBtn?: boolean;
 }
 
 export function Header({ showIconBtn = false }: HeaderProps) {
+  const navigation = useNavigation();
+  
+  function handleGoHome() {
+    navigation.navigate('groups');
+  }
+
   return (
     <Container>
       {showIconBtn && (
-        <IconBtn>
+        <IconBtn onPress={handleGoHome}>
           <LeftArrowIcon />
         </IconBtn>
       )}
