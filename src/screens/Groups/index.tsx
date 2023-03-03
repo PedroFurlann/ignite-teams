fb940a4cd4561cdc9261212af46d07765072ef77
Pedro Furlan <pedrofurlan6@gmail.com>
2023-03-03 09:56:17 -0300

    Update index.tsx

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,12 +3,19 @@ import { EmptyList } from "@components/EmptyList";
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
 import { FlatList } from "react-native";
 import { Container } from "./styles";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
+
+  const navigation = useNavigation();
+
+  function handleCreateNewGroup() {
+    navigation.navigate('new');
+  }
 
   return (
     <Container>
@@ -36,7 +43,7 @@ export function Groups() {
         showsVerticalScrollIndicator={false}
       />
 
-      <Button title="Criar nova turma" />
+      <Button title="Criar nova turma" onPress={handleCreateNewGroup}/>
     </Container>
   )
 } 
