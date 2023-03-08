c37b9899f11a36d9fe0d30a3a6d4ab4587681a5a
Pedro Furlan <pedrofurlan6@gmail.com>
2023-03-08 00:12:50 -0300

    Update Match screen

--- src/screens/Match/index.tsx
+++ src/screens/Match/index.tsx
@@ -1,10 +1,26 @@
+import { ButtonIcon } from "@components/ButtonIcon";
 import { Header } from "@components/Header";
-import { Container } from "./styles";
+import { useState } from "react";
+import { Container, HeaderList, NumberOfPointsContainer, TitleContainer } from "./styles";
 
 export function Match() {
+  const [pointsCounterTeamA, setPointsCounterTeamA] = useState(0);
+  const [pointsCounterTeamB, setPointsCounterTeamB] = useState(0);
+
+
   return (
     <Container>
       <Header />
+      <HeaderList>
+        <TitleContainer>Team A</TitleContainer>
+        <ButtonIcon icon="add" type="PRIMARY" />
+        <NumberOfPointsContainer>
+          {pointsCounterTeamA}
+          X
+          {pointsCounterTeamB}
+        </NumberOfPointsContainer>
+        <TitleContainer>Team B</TitleContainer>
+      </HeaderList>
     </Container>
   )
 }
--- src/screens/Match/styles.ts
+++ src/screens/Match/styles.ts
@@ -1,8 +1,34 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${(props) => props.theme.COLORS.GRAY_600};
   padding: 24px;
 `;
+
+export const HeaderList = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-top: 50px;
+`;
+
+export const TitleContainer = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_200};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.MD}px;
+  `}
+  margin-right: 40px;
+  margin-left: 40px;
+`;
+
+export const NumberOfPointsContainer = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_200};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.LG}px;
+  `}
+`;
+
